29ab8c9c1abcb99b54ecd283d98305f7e8640d51
Anthony Leung <anthony.cb.leung@gmail.com>
2022-08-14 23:41:36 +1000

    added additional comments in main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-//define DOM Elements
+// define DOM Elements
 const targetElement = document.querySelector("#app");
 const form = document.querySelector("form");
 const addressInput = document.querySelector(".address");
@@ -9,43 +9,38 @@ function turnResponseIntoJS(responses) {
 }
 
 function handleData(data) {
+  // define each dataset from dual API fetch
   const ethPrice = data[0].ethereum.usd;
   const txnArray = data[1].result;
 
+  // calculate total gas fee and output html
   const totalFees = txnArray
     .map((txn) => txn.gasPrice * txn.gasUsed * Math.pow(10, -18))
     .reduce((acc, current) => acc + current);
   const totalFeesInUsd = totalFees * ethPrice;
-
-  let html = `<h3>This address has spent <span class="highlight">${totalFees.toFixed(
-    5
-  )} 
-  ETH</span> on gas fees.</h3>
-  <h3>That is currently worth <span class="highlight">${totalFeesInUsd.toFixed(
-    2
-  )} USD</span>, 
+  let html = `<h3>This address has spent <span class="highlight">
+  ${totalFees.toFixed(5)} ETH</span> on gas fees.</h3>
+  <h3>That is currently worth <span class="highlight">
+  ${totalFeesInUsd.toFixed(2)} USD</span>, 
   with 1 ETH being worth ${ethPrice} USD</h3>
   `;
 
+  // output individual transaction data to html
   html += "<h2>Transaction History</h2>";
-
   txnArray.forEach((txn) => {
-    //Calcualte Transaction fee (gasPrice is expressed in wei)
-    const transactionFee = txn.gasPrice * txn.gasUsed * Math.pow(10, -18);
-
-    // Convert Unix time to readable format
+    // convert Unix time to readable format
     const unixTimeStamp = txn.timeStamp;
     const milliseconds = unixTimeStamp * 1000;
     const dateObject = new Date(milliseconds);
     const humanDateFormat = dateObject.toLocaleString();
-
+    // calculate each Transaction fee (gasPrice is expressed in wei)
+    const transactionFee = txn.gasPrice * txn.gasUsed * Math.pow(10, -18);
+    // output to html
     html += `
     <div class="transaction">
     <p>Timestamp: ${humanDateFormat}</p>
-    <span>Transaction Hash: </span><a href="https://etherscan.io/tx/${
-      txn.hash
-    }">
-    ${txn.hash}</a>
+    <span>Transaction Hash: </span>
+    <a href="https://etherscan.io/tx/${txn.hash}">${txn.hash}</a>
     <p>Method: ${txn.functionName === "" ? txn.methodId : txn.functionName}</p>
     <p>Transaction Fee: ${transactionFee.toFixed(7)} ETH</p>
     </div>
@@ -54,7 +49,7 @@ function handleData(data) {
 
   targetElement.innerHTML = html;
 
-  // add effect to have the fees fade out
+  // add css effect to have the fees fade out
   const highlightSpan = document.querySelectorAll(".highlight");
   highlightSpan.forEach((span) => {
     span.addEventListener("mouseover", () => {
@@ -68,11 +63,11 @@ function addressLookup(event) {
   // add loading text
   targetElement.innerHTML = "<h2>Loading...</h2>";
 
-  // Define endpoint for Ethereum price
+  // define endpoint for Ethereum price
   const priceEndpoint =
     "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
 
-  // Define endpoint for Transaction data
+  // define endpoint for Transaction data
   const url = "https://api.etherscan.io/api";
   const parameters = `?module=account&action=txlist&address=${
     addressInput.value
